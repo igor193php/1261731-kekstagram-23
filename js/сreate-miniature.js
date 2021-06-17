ecd31959f67c231d775212569cdbe598ca6203f2
igor193php <iigor193@yandex.ru>
2021-06-18 00:28:13 +0300

    шаг1

--- js/сreate-miniature.js
+++ js/сreate-miniature.js
@@ -1,15 +1,28 @@
+import {createData} from './create-data.js';
+
 const createMiniature = () => {
-const pictureElement = document.querySelector('.pictures');
-const pictureTemplate = document.querySelector('#picture');
-const fragment = document.createDocumentFragment();
 
-for (let i = 0; i < 3; i++) {
+  const data = createData();
+  const picturesElement = document.querySelector('.pictures');
+  const pictureTemplate = document.querySelector('#picture').content;
+  const pictureElement = pictureTemplate.querySelector('.picture');
+  console.log(pictureElement);
+  console.log(picturesElement);
+  const imgElement = pictureElement.querySelector('.picture__img');
+  const pictureInfoElement = pictureElement.querySelector('.picture__info');
+  const pictureLikesElement = pictureInfoElement.querySelector('.picture__likes');
+  const pictureComments = pictureInfoElement.querySelector('.picture__comments');
+  const fragment = document.createDocumentFragment();
 
-}
+  for (let ind = 0; ind < data.length; ind++) {
+    const element = pictureElement.cloneNode(true);
+    imgElement.setAttribute('src', data[0].url);
+    pictureLikesElement.textContent = data[0].likes;
+    pictureComments.textContent = data[0].comments.length;
+    fragment.appendChild();
+  }
 
-//insertAdjacentHTML('beforeend')
-pictureTemlate.appendChild(fragment);
-return pictureTemlate;
+  return pictureTemlate;
 };
 
 export {createMiniature};
