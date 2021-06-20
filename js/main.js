b938e2c9bf51b2a2be74f33ebfc8c6bbce1aa52e
igor193php <iigor193@yandex.ru>
2021-06-20 23:37:12 +0300

    создание карточке в мэйн

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 import {createData} from './create-data.js';
 import {createMiniature} from './сreate-miniature.js';
 
+const data = createData();
+const picturesElement = document.querySelector('.pictures');
+const fragment = document.createDocumentFragment();
+
+for (let i = 0; i < data.length; i++) {
+  const element = createMiniature().cloneNode(true);
+  element.querySelector('.picture__img').setAttribute('src', data[i].url);
+  element.querySelector('.picture__likes').textContent = data[i].likes;
+  element.querySelector('.picture__comments').textContent = data[i].comments.length;
+  fragment.appendChild(element);
+}
+
+picturesElement.appendChild(fragment);
+
 window.console.log(createData());
 window.console.log(createMiniature());
--- js/сreate-miniature.js
+++ js/сreate-miniature.js
@@ -1,22 +1,8 @@
-import {createData} from './create-data.js';
-
 const createMiniature = () => {
 
-  const data = createData();
-  const picturesElement = document.querySelector('.pictures');
   const pictureTemplate = document.querySelector('#picture').content;
-  const pictureElement = pictureTemplate.querySelector('.picture');
-  const fragment = document.createDocumentFragment();
 
-  for (let ind = 0; ind < data.length; ind++) {
-    const element = pictureElement.cloneNode(true);
-    element.querySelector('.picture__img').setAttribute('src', data[ind].url);
-    element.querySelector('.picture__likes').textContent = data[ind].likes;
-    element.querySelector('.picture__comments').textContent = data[ind].comments.length;
-    fragment.appendChild(element);
-  }
-
-  return picturesElement.appendChild(fragment);
+  return pictureTemplate.querySelector('.picture');
 };
 
 export {createMiniature};
