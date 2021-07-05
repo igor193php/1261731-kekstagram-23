b8371fee42003c7661e9c70b913d9500d99f0370
igor193php <iigor193@yandex.ru>
2021-07-05 23:59:41 +0300

    исправлены ошибки

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const MAX_TEXT_LENGTH = 140;
+const MAX_TAG_LENGTH = 20;
+const MAX_TAGS_AMOUNT = 5;
+
 import {createData} from './create-data.js';
 import {createMiniature} from './сreate-miniature.js';
 import {createForm} from './create-form.js';
@@ -40,35 +44,23 @@ const openWindow = () => {
   document.addEventListener('keydown', onPopupEscKeydown);
 };
 
-uploadFileElement.addEventListener('change', () => {
-  openWindow();
-});
-
-uploadCancelElement.addEventListener('click', () => {
-  closeWindow();
-});
-
-const MAX_TEXT_LENGTH = 140;
-const MAX_TEG_LENGTH = 20;
-const MAX_TEGS = 5;
-
 textHashtagsElement.addEventListener('input', () => {
   const valueTextHashtags = textHashtagsElement.value;
-  let arrayTages = valueTextHashtags.split(' ');
-  arrayTages = arrayTages.map((x) => x.toUpperCase());
+  let tags = valueTextHashtags.split(' ');
+  tags = tags.map((x) => x.toUpperCase());
 
-  arrayTages.forEach((item) => {
-    if (item.length > MAX_TEG_LENGTH) {
-      textHashtagsElement.setCustomValidity(`Удалите лишние ${ item.length - MAX_TEG_LENGTH } симв.`);
+  tags.forEach((item) => {
+    if (item.length > MAX_TAG_LENGTH) {
+      textHashtagsElement.setCustomValidity(`Удалите лишние ${ item.length - MAX_TAG_LENGTH } симв.`);
     } else if (!item.includes('#')) {
       textHashtagsElement.setCustomValidity('Начните с #');
-    } else if (item.substring(1, 3) === '') {
+    } else if (item === '#') {
       textHashtagsElement.setCustomValidity('Тег не должен состоять только из #');
     } else if (!regExp('^#[A-Za-zА-Яа-я0-9]{1,19}$').test(item)) {
       textHashtagsElement.setCustomValidity('Название содержит недопустимые символы');
-    } else if (arrayTages.length > MAX_TEGS) {
+    } else if (tags.length > MAX_TAGS_AMOUNT) {
       textHashtagsElement.setCustomValidity('тегов не д.б больше 5');
-    } else if (arrayTages.includes(item, 0)) {
+    } else if (tags.includes(item, 1)) {
       textHashtagsElement.setCustomValidity('Тег дублируется');
     } else {
       textHashtagsElement.setCustomValidity('');
@@ -94,4 +86,12 @@ textDescriptionElement.addEventListener('keydown', (evt) => {
 
 textHashtagsElement.addEventListener('keydown', (evt) => {
   evt.stopPropagation();
+});
+
+uploadFileElement.addEventListener('change', () => {
+  openWindow();
+});
+
+uploadCancelElement.addEventListener('click', () => {
+  closeWindow();
 });
